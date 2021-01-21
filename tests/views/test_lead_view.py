68944d04d43be4816b7cffa87c6654521a181d45
felipe <felipematheusmellodemorais@gmail.com>
2021-01-21 08:10:57 -0300

    feat: function to generate fiels to test were created

--- tests/views/test_lead_view.py
+++ tests/views/test_lead_view.py
@@ -1,4 +1,16 @@
 from flask import json
+import random
+
+def generate_fiels(max):
+    return (''.join([
+        random.choice("abcdefghijqlimnopkrstuvxwyz")
+        for _ in range(max)
+        ]) + '@gmail.com', 
+        ''.join([random.choice('abcdefghijqlimnopkrstuvxwyz') for _ in range(max)
+        ]),
+        ''.join([random.choice(1234567890) for _ in range(max)
+        ])
+    )
 
 
 def new_lead_json():
@@ -12,18 +24,19 @@ def new_lead_json():
         "energy_id": 1,
     }
 
-
+##name, email, phone
 def test_create_new_lead(client):
     """cria novo lead e verifica o retorno do post e o status"""
 
     new_lead = new_lead_json()
 
-    # response = client.post('/lead', json=new_lead)
+    response = client.post('/lead', json=new_lead)
 
-    # data = json.loads(response.data)
-    # status = response.status_code
+    data = json.loads(response.data)
+    status = response.status_code
 
-    # expected = {"data": new_lead}
-
-    # assert data == expected
-    # assert status == 201
+    expected = {"data": new_lead}
+    print(generate_fiels)
+    
+    assert data == expected
+    assert status == 201
